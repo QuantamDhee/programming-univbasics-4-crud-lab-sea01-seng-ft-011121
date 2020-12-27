e42dce19935283c0ba641802ec7c11c5feb0f0e5
Johndhee Ballesteros <johndheeballesteros13@gmail.com>
2020-12-27 16:49:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   animals = ["wolf", "cat", "dog", "cow"]
-  animals.unshift = "wow"
+  animals.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
